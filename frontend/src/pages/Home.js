de6f59fe7e705abd2d81ed119e5c5e92da94180f
Eilan <eilanlaken@gmail.com>
2023-07-03 00:56:51 -0500

    frontend: cleaned unused components and other garbage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,39 +1,9 @@
-import { useEffect, useState } from "react";
-import DevcardThumbnail from "../components/DevcardThumbnail";
-import DevcardForm from "../components/DevcardForm";
+import { useEffect } from "react";
 
 const Home = () => {
-  const [users, setUsers] = useState(null);
+  useEffect(() => {}, []);
 
-  const getAllUsers = async () => {
-    const response = await fetch("http://localhost:4000/users");
-    const json = await response.json();
-    if (response.ok) {
-      setUsers(json.msg);
-      console.log(json.msg.length);
-    }
-  };
-
-  useEffect(() => {
-    getAllUsers();
-  }, []);
-
-  return (
-    <div className="home">
-      <DevcardForm getAllUsers={getAllUsers} />
-      {users &&
-        users.map((user, index) => (
-          <DevcardThumbnail
-            key={index}
-            firstName={user.firstName}
-            lastName={user.lastName}
-            title={user.title}
-            handle={user.handle}
-            img={user.img}
-          />
-        ))}
-    </div>
-  );
+  return <div className="home"></div>;
 };
 
 export default Home;
