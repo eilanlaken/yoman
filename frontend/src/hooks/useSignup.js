8abf52a6d9512b38d312eefd81be9c7a7eaab8ed
Eilan <eilanlaken@gmail.com>
2023-07-03 22:43:31 -0500

    frontend: finished useSignupHook; backend: added .env files for dev and prod, modified scripts in package.json

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from "axios";
 
 const useSignup = () => {
   const [serverError, setServerError] = useState(null);
@@ -8,6 +9,36 @@ const useSignup = () => {
   const signup = async (email, password, firstName, lastName) => {
     setIsLoading(true);
     setServerError(null);
+
+    axios({
+      method: "post",
+      url: `${process.env.REACT_APP_BASE_URL}/auth/signup`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: { email, password, firstName, lastName },
+      timeout: 6000, // 6 seconds
+    })
+      .then((response) => {
+        setIsLoading(false);
+        setSuccess(true);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        setSuccess(false);
+        if (error.response) {
+          // response with error code
+          setServerError(error.response.data.error);
+        } else if (error.request) {
+          // no response at all
+          setServerError(
+            "This didn't work. I am working to fix it. Please come back later"
+          );
+        } else {
+          // other trouble
+          setServerError("Error sending the request.");
+        }
+      });
 
     // const response = await fetch(
     //   `${process.env.REACT_APP_BASE_URL}/auth/signup`,
@@ -28,11 +59,11 @@ const useSignup = () => {
     //   setSuccess(true);
     // }
 
-    setTimeout(() => {
-      setIsLoading(false);
-      setSuccess(true);
-      setServerError(null);
-    }, 200);
+    // setTimeout(() => {
+    //   setIsLoading(false);
+    //   setSuccess(true);
+    //   setServerError(null);
+    // }, 200);
   };
 
   return { signup, isLoading, serverError, success };
